fd5be005cdbe0ea48cd21282bc518ce9e2a88ae7
Viona Septiavi <gravzmedia@gmail.com>
2025-01-16 06:06:08 +0700

    first commit

---
+++ story_content/user.js
@@ -0,0 +1,664 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6CRyCLXGCbX":
+        Script1();
+        break;
+      case "6W7jMs4mCwT":
+        Script2();
+        break;
+      case "6Qm5ONq3Qrn":
+        Script3();
+        break;
+      case "6QIUmXt7IWd":
+        Script4();
+        break;
+      case "5dcey7YvZNZ":
+        Script5();
+        break;
+      case "6YrCOMilLCI":
+        Script6();
+        break;
+      case "5erstDCG5Un":
+        Script7();
+        break;
+      case "5oIrLpBeuvO":
+        Script8();
+        break;
+      case "5WlEknwMogF":
+        Script9();
+        break;
+      case "6jWy5eIEd3z":
+        Script10();
+        break;
+      case "6STi0uaHeE2":
+        Script11();
+        break;
+      case "6ZELHc0gSXz":
+        Script12();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio2 = document.getElementById('bgm2');
+if (audio2) {
+    audio2.pause(); // Menghentikan audio di SKRIP 1
+    audio2.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio3 = document.getElementById('bgm3');
+if (audio3) {
+    audio3.pause(); // Menghentikan audio di SKRIP 1
+    audio3.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio4 = document.getElementById('bgm4');
+if (audio4) {
+    audio4.pause(); // Menghentikan audio di SKRIP 1
+    audio4.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio5 = document.getElementById('bgm5');
+if (audio5) {
+    audio5.pause(); // Menghentikan audio di SKRIP 1
+    audio5.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio6 = document.getElementById('bgm6');
+if (audio6) {
+    audio6.pause(); // Menghentikan audio di SKRIP 1
+    audio6.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio7 = document.getElementById('bgm7');
+if (audio7) {
+    audio7.pause(); // Menghentikan audio di SKRIP 1
+    audio7.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio8 = document.getElementById('bgm8');
+if (audio8) {
+    audio8.pause(); // Menghentikan audio di SKRIP 1
+    audio8.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio1 = document.getElementById('bgm1');
+audio1.src="bgm_main_menu.mp3";
+audio1.load();
+audio1.volume = 0.1;
+audio1.play();
+}
+
+function Script2()
+{
+  var audio1 = document.getElementById('bgm1');
+if (audio1) {
+    audio1.pause(); // Menghentikan audio di SKRIP 1
+    audio1.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio2 = document.getElementById('bgm2');
+if (audio2) {
+    audio2.pause(); // Menghentikan audio di SKRIP 1
+    audio2.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio4 = document.getElementById('bgm4');
+if (audio4) {
+    audio4.pause(); // Menghentikan audio di SKRIP 1
+    audio4.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio5 = document.getElementById('bgm5');
+if (audio5) {
+    audio5.pause(); // Menghentikan audio di SKRIP 1
+    audio5.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio6 = document.getElementById('bgm6');
+if (audio6) {
+    audio6.pause(); // Menghentikan audio di SKRIP 1
+    audio6.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio7 = document.getElementById('bgm7');
+if (audio7) {
+    audio7.pause(); // Menghentikan audio di SKRIP 1
+    audio7.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio8 = document.getElementById('bgm8');
+if (audio8) {
+    audio8.pause(); // Menghentikan audio di SKRIP 1
+    audio8.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio3 = document.getElementById('bgm3');
+audio3.src = "bgm_story2.mp3";
+audio3.load();
+audio3.volume = 0.2;
+audio3.play();
+}
+
+function Script3()
+{
+  var audio1 = document.getElementById('bgm1');
+if (audio1) {
+    audio1.pause(); // Menghentikan audio di SKRIP 1
+    audio1.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio2 = document.getElementById('bgm2');
+if (audio2) {
+    audio2.pause(); // Menghentikan audio di SKRIP 1
+    audio2.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio4 = document.getElementById('bgm4');
+if (audio4) {
+    audio4.pause(); // Menghentikan audio di SKRIP 1
+    audio4.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio5 = document.getElementById('bgm5');
+if (audio5) {
+    audio5.pause(); // Menghentikan audio di SKRIP 1
+    audio5.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio6 = document.getElementById('bgm6');
+if (audio6) {
+    audio6.pause(); // Menghentikan audio di SKRIP 1
+    audio6.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio7 = document.getElementById('bgm7');
+if (audio7) {
+    audio7.pause(); // Menghentikan audio di SKRIP 1
+    audio7.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio8 = document.getElementById('bgm8');
+if (audio8) {
+    audio8.pause(); // Menghentikan audio di SKRIP 1
+    audio8.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio3 = document.getElementById('bgm3');
+audio3.src = "bgm_story2.mp3";
+audio3.load();
+audio3.volume = 0.5;
+audio3.play();
+
+}
+
+function Script4()
+{
+  var audio2 = document.getElementById('bgm2');
+if (audio2) {
+    audio2.pause(); // Menghentikan audio di SKRIP 1
+    audio2.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio3 = document.getElementById('bgm3');
+if (audio3) {
+    audio3.pause(); // Menghentikan audio di SKRIP 1
+    audio3.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio1 = document.getElementById('bgm1');
+if (audio1) {
+    audio1.pause(); // Menghentikan audio di SKRIP 1
+    audio1.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio6 = document.getElementById('bgm6');
+if (audio6) {
+    audio6.pause(); // Menghentikan audio di SKRIP 1
+    audio6.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio5 = document.getElementById('bgm5');
+if (audio5) {
+    audio5.pause(); // Menghentikan audio di SKRIP 1
+    audio5.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio7 = document.getElementById('bgm7');
+if (audio7) {
+    audio7.pause(); // Menghentikan audio di SKRIP 1
+    audio7.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio8 = document.getElementById('bgm8');
+if (audio8) {
+    audio8.pause(); // Menghentikan audio di SKRIP 1
+    audio8.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio4 = document.getElementById('bgm4');
+audio4.src = "bgm_level1.mp3";
+audio4.load();
+audio4.volume = 0.5;
+audio4.play();
+
+}
+
+function Script5()
+{
+  var audio1 = document.getElementById('bgm1');
+if (audio1) {
+    audio1.pause(); // Menghentikan audio di SKRIP 1
+    audio1.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio2 = document.getElementById('bgm2');
+if (audio2) {
+    audio2.pause(); // Menghentikan audio di SKRIP 1
+    audio2.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio4 = document.getElementById('bgm4');
+if (audio4) {
+    audio4.pause(); // Menghentikan audio di SKRIP 1
+    audio4.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio5 = document.getElementById('bgm5');
+if (audio5) {
+    audio5.pause(); // Menghentikan audio di SKRIP 1
+    audio5.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio6 = document.getElementById('bgm6');
+if (audio6) {
+    audio6.pause(); // Menghentikan audio di SKRIP 1
+    audio6.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio7 = document.getElementById('bgm7');
+if (audio7) {
+    audio7.pause(); // Menghentikan audio di SKRIP 1
+    audio7.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio8 = document.getElementById('bgm8');
+if (audio8) {
+    audio8.pause(); // Menghentikan audio di SKRIP 1
+    audio8.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio3 = document.getElementById('bgm3');
+audio3.src = "bgm_story2.mp3";
+audio3.load();
+audio3.volume = 0.5;
+audio3.play();
+
+}
+
+function Script6()
+{
+  var audio2 = document.getElementById('bgm2');
+if (audio2) {
+    audio2.pause(); // Menghentikan audio di SKRIP 1
+    audio2.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio3 = document.getElementById('bgm3');
+if (audio3) {
+    audio3.pause(); // Menghentikan audio di SKRIP 1
+    audio3.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio1 = document.getElementById('bgm1');
+if (audio1) {
+    audio1.pause(); // Menghentikan audio di SKRIP 1
+    audio1.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio6 = document.getElementById('bgm6');
+if (audio6) {
+    audio6.pause(); // Menghentikan audio di SKRIP 1
+    audio6.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio4 = document.getElementById('bgm4');
+if (audio4) {
+    audio4.pause(); // Menghentikan audio di SKRIP 1
+    audio4.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio7 = document.getElementById('bgm7');
+if (audio7) {
+    audio7.pause(); // Menghentikan audio di SKRIP 1
+    audio7.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio8 = document.getElementById('bgm8');
+if (audio8) {
+    audio8.pause(); // Menghentikan audio di SKRIP 1
+    audio8.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio5 = document.getElementById('bgm5');
+audio5.src = "bgm_level2.mp3";
+audio5.load();
+audio5.volume = 0.5;
+audio5.play();
+
+}
+
+function Script7()
+{
+  var audio1 = document.getElementById('bgm1');
+if (audio1) {
+    audio1.pause(); // Menghentikan audio di SKRIP 1
+    audio1.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio2 = document.getElementById('bgm2');
+if (audio2) {
+    audio2.pause(); // Menghentikan audio di SKRIP 1
+    audio2.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio4 = document.getElementById('bgm4');
+if (audio4) {
+    audio4.pause(); // Menghentikan audio di SKRIP 1
+    audio4.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio5 = document.getElementById('bgm5');
+if (audio5) {
+    audio5.pause(); // Menghentikan audio di SKRIP 1
+    audio5.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio6 = document.getElementById('bgm6');
+if (audio6) {
+    audio6.pause(); // Menghentikan audio di SKRIP 1
+    audio6.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio7 = document.getElementById('bgm7');
+if (audio7) {
+    audio7.pause(); // Menghentikan audio di SKRIP 1
+    audio7.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio8 = document.getElementById('bgm8');
+if (audio8) {
+    audio8.pause(); // Menghentikan audio di SKRIP 1
+    audio8.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio3 = document.getElementById('bgm3');
+audio3.src = "bgm_story2.mp3";
+audio3.load();
+audio3.volume = 0.5;
+audio3.play();
+
+}
+
+function Script8()
+{
+  var audio2 = document.getElementById('bgm2');
+if (audio2) {
+    audio2.pause(); // Menghentikan audio di SKRIP 1
+    audio2.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio3 = document.getElementById('bgm3');
+if (audio3) {
+    audio3.pause(); // Menghentikan audio di SKRIP 1
+    audio3.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio1 = document.getElementById('bgm1');
+if (audio1) {
+    audio1.pause(); // Menghentikan audio di SKRIP 1
+    audio1.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio4 = document.getElementById('bgm4');
+if (audio4) {
+    audio4.pause(); // Menghentikan audio di SKRIP 1
+    audio4.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio5 = document.getElementById('bgm5');
+if (audio5) {
+    audio5.pause(); // Menghentikan audio di SKRIP 1
+    audio5.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio7 = document.getElementById('bgm7');
+if (audio7) {
+    audio7.pause(); // Menghentikan audio di SKRIP 1
+    audio7.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio8 = document.getElementById('bgm8');
+if (audio8) {
+    audio8.pause(); // Menghentikan audio di SKRIP 1
+    audio8.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio6 = document.getElementById('bgm6');
+audio6.src = "bgm_level3.mp3";
+audio6.load();
+audio6.volume = 0.06;
+audio6.play();
+
+}
+
+function Script9()
+{
+  var audio1 = document.getElementById('bgm1');
+if (audio1) {
+    audio1.pause(); // Menghentikan audio di SKRIP 1
+    audio1.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio2 = document.getElementById('bgm2');
+if (audio2) {
+    audio2.pause(); // Menghentikan audio di SKRIP 1
+    audio2.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio4 = document.getElementById('bgm4');
+if (audio4) {
+    audio4.pause(); // Menghentikan audio di SKRIP 1
+    audio4.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio5 = document.getElementById('bgm5');
+if (audio5) {
+    audio5.pause(); // Menghentikan audio di SKRIP 1
+    audio5.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio6 = document.getElementById('bgm6');
+if (audio6) {
+    audio6.pause(); // Menghentikan audio di SKRIP 1
+    audio6.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio7 = document.getElementById('bgm7');
+if (audio7) {
+    audio7.pause(); // Menghentikan audio di SKRIP 1
+    audio7.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio8 = document.getElementById('bgm8');
+if (audio8) {
+    audio8.pause(); // Menghentikan audio di SKRIP 1
+    audio8.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio3 = document.getElementById('bgm3');
+audio3.src = "bgm_story2.mp3";
+audio3.load();
+audio3.volume = 0.5;
+audio3.play();
+}
+
+function Script10()
+{
+  var audio2 = document.getElementById('bgm2');
+if (audio2) {
+    audio2.pause(); // Menghentikan audio di SKRIP 1
+    audio2.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio3 = document.getElementById('bgm3');
+if (audio3) {
+    audio3.pause(); // Menghentikan audio di SKRIP 1
+    audio3.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio1 = document.getElementById('bgm1');
+if (audio1) {
+    audio1.pause(); // Menghentikan audio di SKRIP 1
+    audio1.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio4 = document.getElementById('bgm4');
+if (audio4) {
+    audio4.pause(); // Menghentikan audio di SKRIP 1
+    audio4.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio5 = document.getElementById('bgm5');
+if (audio5) {
+    audio5.pause(); // Menghentikan audio di SKRIP 1
+    audio5.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio6 = document.getElementById('bgm6');
+if (audio6) {
+    audio6.pause(); // Menghentikan audio di SKRIP 1
+    audio6.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio8 = document.getElementById('bgm8');
+if (audio8) {
+    audio8.pause(); // Menghentikan audio di SKRIP 1
+    audio8.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+
+var audio7 = document.getElementById('bgm7');
+audio7.src = "bgm_level4.mp3";
+audio7.load();
+audio7.volume = 0.2;
+audio7.play();
+
+}
+
+function Script11()
+{
+  var audio1 = document.getElementById('bgm1');
+if (audio1) {
+    audio1.pause(); // Menghentikan audio di SKRIP 1
+    audio1.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio2 = document.getElementById('bgm2');
+if (audio2) {
+    audio2.pause(); // Menghentikan audio di SKRIP 1
+    audio2.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio4 = document.getElementById('bgm4');
+if (audio4) {
+    audio4.pause(); // Menghentikan audio di SKRIP 1
+    audio4.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio5 = document.getElementById('bgm5');
+if (audio5) {
+    audio5.pause(); // Menghentikan audio di SKRIP 1
+    audio5.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio6 = document.getElementById('bgm6');
+if (audio6) {
+    audio6.pause(); // Menghentikan audio di SKRIP 1
+    audio6.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio7 = document.getElementById('bgm7');
+if (audio7) {
+    audio7.pause(); // Menghentikan audio di SKRIP 1
+    audio7.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio8 = document.getElementById('bgm8');
+if (audio8) {
+    audio8.pause(); // Menghentikan audio di SKRIP 1
+    audio8.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio3 = document.getElementById('bgm3');
+audio3.src = "bgm_story2.mp3";
+audio3.load();
+audio3.volume = 0.5;
+audio3.play();
+}
+
+function Script12()
+{
+  var audio2 = document.getElementById('bgm2');
+if (audio2) {
+    audio2.pause(); // Menghentikan audio di SKRIP 1
+    audio2.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio3 = document.getElementById('bgm3');
+if (audio3) {
+    audio3.pause(); // Menghentikan audio di SKRIP 1
+    audio3.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio1 = document.getElementById('bgm1');
+if (audio1) {
+    audio1.pause(); // Menghentikan audio di SKRIP 1
+    audio1.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio4 = document.getElementById('bgm4');
+if (audio4) {
+    audio4.pause(); // Menghentikan audio di SKRIP 1
+    audio4.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio5 = document.getElementById('bgm5');
+if (audio5) {
+    audio5.pause(); // Menghentikan audio di SKRIP 1
+    audio5.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio7 = document.getElementById('bgm7');
+if (audio7) {
+    audio7.pause(); // Menghentikan audio di SKRIP 1
+    audio7.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio6 = document.getElementById('bgm6');
+if (audio6) {
+    audio6.pause(); // Menghentikan audio di SKRIP 1
+    audio6.currentTime = 0; // Mengatur ulang audio di SKRIP 1 ke awal
+}
+
+var audio8 = document.getElementById('bgm8');
+audio8.src = "bgm_level5_2.mp3";
+audio8.load();
+audio8.volume = 0.1;
+audio8.play();
+
+}
+
